f94ec3fac85872844837916fe084773736d88151
Chris MacLellan <maclellan.christopher@gmail.com>
2016-05-09 13:59:08 -0400

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a platform for creating apprentice learner agents.
---
+++ apprentice_learner/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApprenticeLearnerConfig(AppConfig):
+    name = 'apprentice_learner'
