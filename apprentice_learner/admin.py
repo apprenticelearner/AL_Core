2136d49b2e591e5ec066c200fd3e384d9b36cfb4
Chris MacLellan <maclellan.christopher@gmail.com>
2016-05-24 12:05:13 -0400

    added admin page

--- apprentice_learner/admin.py
+++ apprentice_learner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apprentice_learner.models import Agent
+
+class AgentAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Agent, AgentAdmin)
