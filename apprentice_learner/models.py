fc8f09b035a45656361d3ad1f83ef0cab9a88649
anant-dadu <anant.dadu.cse14@itbhu.ac.in>
2016-05-26 11:22:12 -0400

    updated models

--- apprentice_learner/models.py
+++ apprentice_learner/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 from picklefield.fields import PickledObjectField
 
+class PyFunction(models.Model):
+    name = models.CharField(max_length=200)
+    fun_def = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class ActionSet(models.Model):
+    name = models.CharField(max_length=200)
+    features = models.ManyToManyField(PyFunction, blank=True, related_name="feature_action_sets")
+    function = models.ManyToManyField(PyFunction, blank=True, related_name="function_action_sets")
+    def __str__(self):
+        return self.name
+
 class Agent(models.Model):
     instance = PickledObjectField()
-    # actions set
-    # number of each type of request (request, train, check)
-    # maybe past performance
-    # time of creation
-    # last update time
+    action_set = models.ForeignKey(ActionSet, on_delete=models.CASCADE)
+    name = models.CharField("agent's name", max_length=200)
+    num_request = models.IntegerField(default=0) 
+    num_train = models.IntegerField(default=0)
+    num_check = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+   
     # user specified domain
     # owner
-
-# Actions sets
-    # functions
-    # features
-
-# Python Functions
 
 # Users and User permissions
 
