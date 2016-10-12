0510c0a1ef63ff2e9362717b0c81d12259cbf7f2
eharpste <eharpste@cs.cmu.edu>
2016-10-12 16:10:13 -0400

    expanded the create view to capture an agent name if provided and created new url patterns and views for requesting, training, and checking with agents by name

--- apprentice_learner/urls.py
+++ apprentice_learner/urls.py
@@ -4,9 +4,13 @@ from django.conf.urls.static import static
 
 from apprentice_learner import views
 
+app_name = 'apprentice_api'
 urlpatterns = [
 	url(r'^create/$', views.create, name = "create"),
-	url(r'^request/(?P<agent_id>.+)/$', views.request, name="request"),
-	url(r'^train/(?P<agent_id>.+)/$', views.train, name="train"),
-	url(r'^check/(?P<agent_id>.+)/$', views.check, name="check")
+	url(r'^request/(?P<agent_id>[0-9]+)/$', views.request, name="request"),
+	url(r'^train/(?P<agent_id>[0-9]+)/$', views.train, name="train"),
+	url(r'^check/(?P<agent_id>[0-9]+)/$', views.check, name="check"),
+    url(r'^request/(?P<agent_name>[a-zA-Z0-9_-]{1,200})/$', views.request_by_name, name="request_by_name"),
+    url(r'^train/(?P<agent_name>[a-zA-Z0-9_-]{1,200})/$', views.train_by_name, name="train_by_name"),
+    url(r'^check/(?P<agent_name>[a-zA-Z0-9_-]{1,200})/$', views.check_by_name, name="check_by_name"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
