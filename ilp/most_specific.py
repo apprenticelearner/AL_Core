95d2250429cc4e296dc01293bf0b3ad2f542580b
Chris MacLellan <maclellan.christopher@gmail.com>
2017-02-08 16:56:02 -0500

    updated to support pos and neg examples

--- ilp/most_specific.py
+++ ilp/most_specific.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 
 from concept_formation.cobweb3 import Cobweb3Node
+from concept_formation.cobweb import CobwebNode
+from concept_formation.preprocessor import NameStandardizer
 from concept_formation.structure_mapper import StructureMapper
 from concept_formation.structure_mapper import rename_flat
 from concept_formation.structure_mapper import contains_component
@@ -8,6 +10,7 @@ from concept_formation.continuous_value import ContinuousValue
 from ilp.fo_planner import Operator
 from ilp.fo_planner import build_index
 from ilp.fo_planner import subst
+from ilp.fo_planner import unify
 
 from ilp.base_ilp import BaseILP
 
@@ -27,6 +30,19 @@ def gensym():
     return '?o' + str(my_gensym_counter)
 
 
+def get_vars(arg):
+    if isinstance(arg, tuple):
+        l = []
+        for e in arg:
+            for v in get_vars(e):
+                if v not in l:
+                    l.append(v)
+        return l
+    elif isinstance(arg, str) and len(arg) > 0 and arg[0] == '?':
+        return [arg]
+    else:
+        return []
+
 class MostSpecific(BaseILP):
 
     def __init__(self):
@@ -43,7 +59,7 @@ class MostSpecific(BaseILP):
 
     def ifit(self, t, x, y):
         self.target_types = t
-        
+
         if y == 1:
             self.pos.add(tuple(t))
 
@@ -87,11 +103,35 @@ class SimStudentWhere(BaseILP):
     def __init__(self):
         self.pos = set()
         self.operator = None
-        self.count = 0
         self.concept = Cobweb3Node()
+        self.pos_concept = CobwebNode()
+        self.neg_concept = CobwebNode()
 
     def __len__(self):
         return self.count
+
+    # def __repr__(self):
+    #     return repr(self.operator)
+
+    def check_match(self, t, x):
+        """
+        if y is predicted to be 1 then returns True
+        else returns False
+        """
+        if self.operator is None:
+            return
+
+        mapping = {a: t[i] for i, a in enumerate(self.operator.name[1:])}
+
+        print("MY MAPPING", mapping)
+
+        # print("GETTING MATCHES")
+        grounded = [(ground(a), x[a]) for a in x if (isinstance(a, tuple))]
+        index = build_index(grounded)
+
+        for m in self.operator.match(index, initial_mapping=mapping):
+            return True
+        return False
 
     def get_matches(self, x, constraints=None):
         if self.operator is None:
@@ -102,6 +142,7 @@ class SimStudentWhere(BaseILP):
         index = build_index(grounded)
 
         for m in self.operator.match(index):
+            # print('match', m, self.operator.name)
             result = tuple(['?' + subst(m, ele)
                             for ele in self.operator.name[1:]])
             # result = tuple(['?' + m[e] for e in self.target_types])
@@ -132,12 +173,17 @@ class SimStudentWhere(BaseILP):
                 return True
         return False
 
+    def is_structural_feature(self, attr, value):
+        if not isinstance(attr, tuple) or attr[0] != 'value':
+            return True
+        print('REMOVING: ', attr, value)
+        return False
+
     def ifit(self, t, x, y):
-        if y == 0:
-            return
+        # if y == 0:
+        #     return
 
-        self.count += 1
-
+        # x = {a: x[a] for a in x if self.is_structural_feature(a, x[a])}
         x = {a: x[a] for a in x}
 
         # eles = set([field for field in t])
@@ -168,48 +214,62 @@ class SimStudentWhere(BaseILP):
 
         # print("adding:")
 
-        sm = StructureMapper(self.concept, gensym=gensym)
-        x = sm.transform(x)
+        ns = NameStandardizer()
+        sm = StructureMapper(self.concept)
+        x = sm.transform(ns.transform(x))
         # pprint(x)
         self.concept.increment_counts(x)
 
+        if y == 1:
+            self.pos_concept.increment_counts(x)
+        else:
+            self.neg_concept.increment_counts(x)
+
+        # print()
+        # print('POSITIVE')
+        # pprint(self.pos_concept.av_counts)
+        # print('NEGATIVE')
+        # pprint(self.neg_concept.av_counts)
+
         # pprint(self.concept.av_counts)
 
-        remove = []
-        for attr in self.concept.av_counts:
-            if len(self.concept.av_counts[attr]) > 1:
-                remove.append(attr)
-            elif isinstance(self.concept.av_counts[attr], ContinuousValue):
-                if (self.concept.av_counts[attr].biased_std() != 0 or
-                    self.concept.av_counts[attr].num != self.concept.count):
-                    remove.append(attr)
-            else:
-                for val in self.concept.av_counts[attr]:
-                    if self.concept.av_counts[attr][val] != self.concept.count:
-                        remove.append(attr)
+        pos_instance = {}
+        pos_args = set()
+        for attr in self.pos_concept.av_counts:
+            if len(self.pos_concept.av_counts[attr]) == 1:
+                for val in self.pos_concept.av_counts[attr]:
+                    if ((self.pos_concept.av_counts[attr][val] ==
+                         self.pos_concept.count)):
 
-        for attr in remove:
-            del self.concept.av_counts[attr]
+                        args = get_vars(attr)
+                        pos_args.update(args)
+                        pos_instance[attr] = val
 
-        pattern_instance = {}
-        for attr in self.concept.av_counts:
-            if isinstance(self.concept.av_counts[attr], ContinuousValue):
-                m = self.concept.av_counts[attr].unbiased_mean()
-                pattern_instance[attr] = m
-            else:
-                for val in self.concept.av_counts[attr]:
-                    pattern_instance[attr] = val
-        # pattern_instance = {attr: val for attr in self.concept.av_counts
-        #                     for val in self.concept.av_counts[attr]}
-        pprint(pattern_instance)
+        # print('POS ARGS', pos_args)
+
+        neg_instance = {}
+        for attr in self.neg_concept.av_counts:
+            args = set(get_vars(attr))
+            if not args.issubset(pos_args):
+                continue
+
+            for val in self.neg_concept.av_counts[attr]:
+                if ((attr not in self.pos_concept.av_counts or val not in
+                     self.pos_concept.av_counts[attr])):
+                    neg_instance[attr] = val
+
         foa_mapping = {'foa%s' % j: '?foa%s' % j for j in range(len(t))}
-        pattern_instance = rename_flat(pattern_instance, foa_mapping)
-        
+        pos_instance = rename_flat(pos_instance, foa_mapping)
+        neg_instance = rename_flat(neg_instance, foa_mapping)
+
+        conditions = ([(a, pos_instance[a]) for a in pos_instance] +
+                      [('not', (a, neg_instance[a])) for a in neg_instance])
+
+        # pprint(conditions)
 
         self.target_types = ['?foa%s' % i for i in range(len(t))]
         self.operator = Operator(tuple(['Rule'] + self.target_types),
-                                 [(a, pattern_instance[a])
-                                  for a in pattern_instance], [])
+                                 conditions, [])
 
     def fit(self, T, X, y):
         self.count = len(X)
@@ -224,3 +284,37 @@ class SimStudentWhere(BaseILP):
 
         for i, t in enumerate(T):
             self.ifit(t, X[i], y[i])
+
+
+if __name__ == "__main__":
+
+    ssw = SimStudentWhere()
+
+    p1 = {('on', '?o1', '?o2'): True,
+          ('name', '?o1'): "Block 1",
+          ('name', '?o2'): "Block 7",
+          ('value', '?o2'): 99}
+
+    p2 = {('on', '?o3', '?o4'): True,
+          ('name', '?o3'): "Block 3",
+          ('name', '?o4'): "Block 4"}
+
+    n1 = {('on', '?o5', '?o6'): True,
+          ('on', '?o6', '?o7'): True,
+          ('name', '?o5'): "Block 5",
+          ('name', '?o6'): "Block 6",
+          ('name', '?o7'): "Block 7",
+          ('value', '?o7'): 100}
+
+    ssw.ifit(['?o1'], p1, 1)
+    ssw.ifit(['?o3'], p2, 1)
+    ssw.ifit(['?o5'], n1, 0)
+
+    test1 = {('on', '?o5', '?o6'): True,
+             ('on', '?o6', '?o7'): True,
+             ('name', '?o5'): "Block 1",
+             ('name', '?o6'): "Block 7",
+             ('name', '?o7'): "Block 5"}
+
+    for m in ssw.get_matches(test1):
+        print('OP MATCH', m)
