6ae43e989f85d206d0eb56a7754fa9b0434505a1
Danny Weitekamp <dannyweitekamp@gmail.com>
2019-07-25 15:34:33 -0400

    needed to explicit import after-all

--- planners/base_planner.py
+++ planners/base_planner.py
@@ -13,6 +13,8 @@ class BasePlanner(object):
 
 
 def get_planner_class(name):
+	if(name == "vectorized"):
+		from planners.VectorizedPlanner import VectorizedPlanner
 	name = name.lower().replace(' ', '').replace('_', '')
 	return PLANNERS[name]
 
