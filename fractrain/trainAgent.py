2b2f7014006371552275a1d3b60bc101ad7a3a5c
Gabriel Brookman <gabriel@eduroam-165-25.dyn.carleton.edu>
2019-08-20 13:21:04 -0500

    added documentation

--- fractrain/trainAgent.py
+++ fractrain/trainAgent.py
@@ -34,7 +34,7 @@ def log_rules(state):
 
 bignums = []
 
-for i in range(1000):
+for i in range(1):
     xn = randrange(1,10)
     yn = randrange(1,10)
     xd = randrange(1,10)
@@ -55,96 +55,88 @@ for i in range(1000):
     yd = randrange(1,10)
     bignums.append([str(xn)+"/"+str(xd)+":"+str(yn)+"/"+str(yd),"Div",str(xn*yd)+"/"+str(xd*yn)])
 
-with open(fdir + fname) as fin:
-    mod = 0
-    for eq in bignums:
-        prob, op, result = eq
-        result = result.replace(" ","")
-        lhs, rhs = prob.split(opsdict[op])
-        num1, denom1 = lhs.split("/")
-        num2, denom2 = rhs.split("/")
-        if not (result.count("/") == 1): continue
-        resultnum, resultdenom = result.split("/")
-        try:
-            int(resultnum)
-            int(resultdenom)
-        except ValueError:
-            continue
-        state = {"num1":{"id":"num1","value":num1,"contentEditable":False},
-            "num2":{"id":"num2","value":num2,"contentEditable":False},
-            "denom1":{"id":"denom1","value":denom1,"contentEditable":False},
-            "denom2":{"id":"denom2","value":denom2,"contentEditable":False},
-            "op":{"id":"op","value":op,"contentEditable":False},
-            "num3":{"id":"num3","value":"","contentEditable":True},
-            "denom3":{"id":"denom3","value":"","contentEditable":False},
-        }
+for eq in bignums:
+    prob, op, result = eq
+    result = result.replace(" ","")
+    lhs, rhs = prob.split(opsdict[op])
+    num1, denom1 = lhs.split("/")
+    num2, denom2 = rhs.split("/")
+    if not (result.count("/") == 1): continue
+    resultnum, resultdenom = result.split("/")
+    try:
+        int(resultnum)
+        int(resultdenom)
+    except ValueError:
+        continue
+    state = {"num1":{"id":"num1","value":num1,"contentEditable":False},
+        "num2":{"id":"num2","value":num2,"contentEditable":False},
+        "denom1":{"id":"denom1","value":denom1,"contentEditable":False},
+        "denom2":{"id":"denom2","value":denom2,"contentEditable":False},
+        "op":{"id":"op","value":op,"contentEditable":False},
+        "num3":{"id":"num3","value":"","contentEditable":True},
+        "denom3":{"id":"denom3","value":"","contentEditable":False},
+    }
 
-        input_for_get = {
-            "states":[
-                   state,
-                ],
-        }
+    input_for_get = {
+        "states":[
+               state,
+            ],
+    }
 
-        url = "http://127.0.0.1:8000/"
-
-        if mod%10 == 9:
-            print(eq)
-            print("new test below")
-            reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
-            print(reqReq.json())
+    url = "http://127.0.0.1:8000/"
+    obj = {
+      "selection": "num3",
+      "action": "UpdateTextField",
+      "inputs": {
+          "value": resultnum
+      },
+      "reward": 1,
+      "state": state
+    }
+    trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
+    reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
+    response = requests.post(url+"request/"+str(agentID)+"/", json={"state":state})
+    numeratorComputed = reqReq.json()["inputs"]["value"]
+    if numeratorComputed != resultnum:
+        print("got num wrong, correcting")
         obj = {
           "selection": "num3",
           "action": "UpdateTextField",
           "inputs": {
-              "value": resultnum
+              "value": numeratorComputed,
           },
-          "reward": 1,
+          "reward": 0,
           "state": state
         }
         trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
-        reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
-        response = requests.post(url+"request/"+str(agentID)+"/", json={"state":state})
-        numeratorComputed = reqReq.json()["inputs"]["value"]
-        if numeratorComputed != resultnum:
-            print("got num wrong, correcting")
-            obj = {
-              "selection": "num3",
-              "action": "UpdateTextField",
-              "inputs": {
-                  "value": numeratorComputed,
-              },
-              "reward": 0,
-              "state": state
-            }
-            trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
-        log_rules(state)
-        state["num3"] = {"id":"num3","value":resultnum,"contentEditable":False}
-        state["denom3"] = {"id":"denom3","value":"","contentEditable":True}
+    log_rules(state)
+    state["num3"] = {"id":"num3","value":resultnum,"contentEditable":False}
+    state["denom3"] = {"id":"denom3","value":"","contentEditable":True}
+    obj = {
+      "selection": "denom3",
+      "action": "UpdateTextField",
+      "inputs": {
+          "value": resultdenom
+      },
+      "reward": 1,
+      "state": state
+    }
+    trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
+    reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
+    print(reqReq.json())
+    denominatorComputed = reqReq.json()["inputs"]["value"]
+    if denominatorComputed != resultdenom:
+        print("got denom wrong, correcting")
         obj = {
           "selection": "denom3",
           "action": "UpdateTextField",
           "inputs": {
-              "value": resultdenom
+              "value": denominatorComputed,
           },
-          "reward": 1,
+          "reward": 0,
           "state": state
         }
         trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
-        reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
-        print(reqReq.json())
-        denominatorComputed = reqReq.json()["inputs"]["value"]
-        if denominatorComputed != resultdenom:
-            print("got denom wrong, correcting")
-            obj = {
-              "selection": "denom3",
-              "action": "UpdateTextField",
-              "inputs": {
-                  "value": denominatorComputed,
-              },
-              "reward": 0,
-              "state": state
-            }
-            trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
-        log_accuracy(prob, resultnum, resultdenom, numeratorComputed, denominatorComputed)
-        log_rules(state)
-        state["denom3"] = {"id":"num3","value":resultdenom,"contentEditable":False}
+    log_accuracy(prob, resultnum, resultdenom, numeratorComputed, denominatorComputed)
+    log_rules(state)
+    state["denom3"] = {"id":"num3","value":resultdenom,"contentEditable":False}
