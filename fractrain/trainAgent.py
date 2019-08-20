b0b3f7a885af4dc97853e21244572b23ed59b9de
Gabriel Brookman <gabriel@eduroam-165-25.dyn.carleton.edu>
2019-08-20 11:06:01 -0500

    now working with logging and rules logging

--- fractrain/trainAgent.py
+++ fractrain/trainAgent.py
@@ -20,6 +20,18 @@ def log_accuracy(prob,resultnum,resultdenom,numeratorComputed,denominatorCompute
     fi.write(("Correct." if eval(resultnum+"/"+resultdenom) == eval(numeratorComputed+"/"+denominatorComputed) else "Incorrect.")+" Problem: "+prob+" Answer Given: "+numeratorComputed+"/"+denominatorComputed+". The correct answer was: "+resultnum+"/"+resultdenom+"\n")
     fi.close()
 
+def log_rules(state):
+    obj = {"states":[state,],}
+    rule = requests.post(url+"get_skills/"+str(agentID)+"/", json=obj)
+    print(rule)
+    print(rule.status_code, rule.reason)
+    rule = rule.json()
+    print("rule learned:")
+    print(rule)
+    fi = open("ruleslog","a+")
+    fi.write(str(rule)+"\n")
+    fi.close()
+
 bignums = []
 
 for i in range(1000):
@@ -27,19 +39,25 @@ for i in range(1000):
     yn = randrange(1,10)
     xd = randrange(1,10)
     yd = randrange(1,10)
-    #bignums.append([str(xn)+"/"+str(xd)+"*"+str(yn)+"/"+str(yd),"Mult",str(xn*yn)+"/"+str(xd*yd)])
+    bignums.append([str(xn)+"/"+str(xd)+"*"+str(yn)+"/"+str(yd),"Mult",str(xn*yn)+"/"+str(xd*yd)])
+    xn = randrange(1,10)
+    yn = randrange(1,10)
+    xd = randrange(1,10)
+    bignums.append([str(xn)+"/"+str(xd)+"+"+str(yn)+"/"+str(xd),"Add",str(xn+yn)+"/"+str(xd)])
     xn = randrange(1,10)
     yn = randrange(1,10)
     xd = randrange(1,10)
     yd = randrange(1,10)
-    bignums.append([str(xn)+"/"+str(xd)+"+"+str(yn)+"/"+str(yd),"Add",str(xn+yn)+"/"+str(xd+yd)])
+    bignums.append([str(xn)+"/"+str(xd)+"-"+str(yn)+"/"+str(xd),"Sub",str(xn-yn)+"/"+str(xd)])
+    xn = randrange(1,10)
+    yn = randrange(1,10)
+    xd = randrange(1,10)
+    yd = randrange(1,10)
+    bignums.append([str(xn)+"/"+str(xd)+":"+str(yn)+"/"+str(yd),"Div",str(xn*yd)+"/"+str(xd*yn)])
 
 with open(fdir + fname) as fin:
     mod = 0
     for eq in bignums:
-    #for eq in fin:
-        #print(eq)
-        #prob, op, result = [x[y] for x in (eq.split(","),) for y in (4,5,8)]
         prob, op, result = eq
         result = result.replace(" ","")
         lhs, rhs = prob.split(opsdict[op])
@@ -61,6 +79,14 @@ with open(fdir + fname) as fin:
             "denom3":{"id":"denom3","value":"","contentEditable":False},
         }
 
+        input_for_get = {
+            "states":[
+                   state,
+                ],
+        }
+
+        url = "http://127.0.0.1:8000/"
+
         if mod%10 == 9:
             print(eq)
             print("new test below")
@@ -78,24 +104,22 @@ with open(fdir + fname) as fin:
         trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
         reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
         response = requests.post(url+"request/"+str(agentID)+"/", json={"state":state})
-        print(response)
-        print(response.status_code, response.reason)
-        print(response.json())
-
-        #response = requests.post(url + "get_skills/"+str(agentID)+"/", json={"states":[state]})
-
-        print(response)
-        print(response.status_code, response.reason)
-        print(response.json())
-
-
-        print(reqReq.json())
         numeratorComputed = reqReq.json()["inputs"]["value"]
-        #print(trainReq.status_code, trainReq.reason)
-        #print(state)
+        if numeratorComputed != resultnum:
+            print("got num wrong, correcting")
+            obj = {
+              "selection": "num3",
+              "action": "UpdateTextField",
+              "inputs": {
+                  "value": numeratorComputed,
+              },
+              "reward": 0,
+              "state": state
+            }
+            trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
+        log_rules(state)
         state["num3"] = {"id":"num3","value":resultnum,"contentEditable":False}
         state["denom3"] = {"id":"denom3","value":"","contentEditable":True}
-        #print(state)
         obj = {
           "selection": "denom3",
           "action": "UpdateTextField",
@@ -106,16 +130,21 @@ with open(fdir + fname) as fin:
           "state": state
         }
         trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
-        if mod%100 == 99:
-            print("new test below")
-            reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
-            print(reqReq.json())
         reqReq = requests.post(url+"request/"+str(agentID)+"/", json={"state": state})
         print(reqReq.json())
         denominatorComputed = reqReq.json()["inputs"]["value"]
+        if denominatorComputed != resultdenom:
+            print("got denom wrong, correcting")
+            obj = {
+              "selection": "denom3",
+              "action": "UpdateTextField",
+              "inputs": {
+                  "value": denominatorComputed,
+              },
+              "reward": 0,
+              "state": state
+            }
+            trainReq = requests.post(url+"train/"+str(agentID)+"/", json=obj)
         log_accuracy(prob, resultnum, resultdenom, numeratorComputed, denominatorComputed)
+        log_rules(state)
         state["denom3"] = {"id":"num3","value":resultdenom,"contentEditable":False}
-        #print(state)
-        #print(trainReq.status_code, trainReq.reason)
-        mod += 1
-        mod %= 100
