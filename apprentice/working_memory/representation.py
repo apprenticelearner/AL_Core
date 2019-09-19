9a342a955875c954e3176965371cd28b49c1b9c1
Ben Mastay <ben.mastay@soartech.com>
2019-09-19 10:15:02 -0400

    merging stubs together, adding type hints

--- apprentice/working_memory/representation.py
+++ apprentice/working_memory/representation.py
@@ -1,11 +1,25 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+from typing import Collection, Callable, Dict
 
 
 # Placeholder for now. What do these actually need to implement?
-
 class Fact(metaclass=ABCMeta):
     pass
 
 
-class Skill(metaclass=ABCMeta):
+class Condition(metaclass=ABCMeta):
     pass
+
+
+class Skill(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def name(self) -> str: ...
+
+    @property
+    @abstractmethod
+    def conditions(self) -> Collection[Condition]: ...
+
+    @abstractmethod
+    def function(self, state: Dict) -> Callable: ...
+
