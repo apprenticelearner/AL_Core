74f20bc77f4ece5a3a77cf4c5291d1d54df3b2bf
cmaclell <maclellan.christopher@gmail.com>
2019-09-20 17:11:11 -0400

    refining soartech agent

--- apprentice/agents/soartech_agent.py
+++ apprentice/agents/soartech_agent.py
@@ -1,4 +1,6 @@
 from typing import Collection
+from typing import Dict
+from random import random
 
 from apprentice.working_memory.representation import Skill
 from apprentice.agents.base import BaseAgent
@@ -11,55 +13,58 @@ class SoarTechAgent(BaseAgent):
 
     def __init__(self, prior_skills: Collection[Skill]):
         # Just track the state as a set of Facts?
+        # initialize to None, so gets replaced on first state.
         self.last_state = None
 
         # Need a working memory class
         self.working_memory = None
+        self.working_memory.add_skills(prior_skills)
 
-        for skill in feature_set:
-            self.working_memory.declare_skill(skill)
-
-    def request(self, state):
+    def select_skill(self, candidate_skills: Collection[Skill]) -> Skill:
         """
-        Accepts a JSON object representing the state.
+        Given a list of candidate skills evaluate them and determines which one
+        has the highest expected rewared in the current state.
 
-        Returns a dictionary containing selection, action, and inputs.
+        .. todo::
+
+            Not sure what a state looks like here? Is it just a collection of
+                facts?
+            Are candidate skills a collection of skills or skill activations?
+            Might also need some kind of strategy for choosing, currently just
+                choosing best.
+
+        :param candidate_skills: Skills being considered for activation
         """
-        if self.last_state is None:
-            return self.request_diff(state, [])
+        # just passing in the working memory facts to each skill, where the
+        # facts is just the current state representation.
+        skills = [(skill.expected_reward(self.working_memory.facts), random(),
+                   skill) for skill in candidate_skills]
+        skills.sort(reverse=True)
+        expected_reward, _, best_skill = skills[0]
+        return best_skill
 
-        pos_diff = state - self.last_state
-        neg_diff = self.last_state - state
-
-        return self.request_diff(pos_diff, neg_diff)
-
-    def request_diff(self, state_pos_diff, state_neg_diff):
+    def request_diff(self, state_diff: Dict):
         """
-        Accepts a JSON object representing diffs from the previously requested
-        state. Useful for more efficiently making requests when the state
-        changes only a little bit.
+        Queries the agent to get an action to execute in the world given the
+        diff from the last state.
 
-        Returns a dictionary containing selection, action, and inputs.
+        :param state_diff: a state diff output from JSON Diff.
         """
-        # relational inference step?
-        # there could be a repeated process of updating, but maybe we wanna
-        # chose the skills that get applied, so do the loop below.
-        self.working_memory.retract(state_neg_diff)
-        self.working_memory.declare(state_pos_diff)
+        # Just loads in the differences from the state diff
+        self.working_memory.update(state_diff)
 
-        while True:
-            candidate_skills = [skill for skill in self.working_memory.agenda]
-            if len(candidate_skills) == 0:
-                break
-            best_skill = select_skill(candidate_skills, state)
-            best_skill.activate()
-            # what if the skills produces external action? Need to return it?
-            # Does this happen within the skill?
-            # Maybe activate returns something?
-            # Maybe we need something special for recognizing SAI facts?
+        # This should do essentially what `engine.run` is doing from
+        # PyKnow. Pyknow currently uses salience to choose rule order, but
+        # we want to essentially set salience using the when learning.
+        while not self.working_memory.output:
+            candidate_activations = [activation for activation in
+                                     self.working_memory.activations]
+            if len(candidate_activations) == 0:
+                return {}
+            best_activation = self.select_activation(candidate_activations)
+            best_activation.fire()
 
-        # return empty action
-        return {}
+        return self.working_memory.output
 
     def train(self, state, selection, action, inputs, reward, skill_label,
               foci_of_attention):
@@ -76,7 +81,7 @@ class SoarTechAgent(BaseAgent):
         neg_diff = self.last_state - state
 
         return self.train_diff(pos_diff, neg_diff, selection, action, inputs,
-                               reward, skill_lable, foci_of_attention)
+                               reward, skill_label, foci_of_attention)
 
     def train_diff(self, state_pos_diff, state_neg_diff, selection, action,
                    inputs, reward, skill_label, foci_of_attention):
