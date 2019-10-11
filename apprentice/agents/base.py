60df72537cc32829652aaf67a82b5e7cf2403851
robert.sheline <robert.sheline@soartech.com>
2019-10-11 02:24:58 -0400

    update handles json replace symbol type

--- apprentice/agents/base.py
+++ apprentice/agents/base.py
@@ -1,13 +1,13 @@
 from abc import ABCMeta, abstractmethod
-from typing import Dict
 from typing import Collection
-from apprentice.working_memory.representation import Skill
+from typing import Dict
 
+from apprentice.working_memory.representation import Skill, Sai
 from jsondiff import diff
 
 
 class BaseAgent(metaclass=ABCMeta):
-    prior_state = None
+    prior_state = {}
 
     def __init__(self, prior_skills: Collection[Skill]):
         """
@@ -21,7 +21,8 @@ class BaseAgent(metaclass=ABCMeta):
 
         :param state: a state represented as a dict (parsed from JSON)
         """
-        return self.request_diff(diff(state, self.prior_state))
+        d = diff(self.prior_state, state)
+        return self.request_diff(d)
 
     @abstractmethod
     def request_diff(self, state_diff: Dict) -> Dict:
@@ -30,21 +31,21 @@ class BaseAgent(metaclass=ABCMeta):
         """
         pass
 
-    def train(self, state: Dict, selection: str, action: str, inputs:
-              Collection[str], reward: float, skill_label: str,
+    def train(self, state: Dict, sai: Sai, reward: float, skill_label: str,
               foci_of_attention: Collection[str]):
         """
-        Accepts a JSON/Dict object representing the state, a string representing the
+        Accepts a JSON/Dict object representing the state, a string
+        representing the
         skill label, a list of strings representing the foas, a string
         representing the selection, a string representing the action, list of
         strings representing the inputs, and a boolean correctness.
         """
-        return self.train_diff(diff(state, self.prior_state), selection,
-                               action, inputs, reward, skill_label,
+        return self.train_diff(diff(self.prior_state, state), sai, reward,
+                               skill_label,
                                foci_of_attention)
 
     @abstractmethod
-    def train_diff(self, state_diff, selection, action, inputs, reward,
+    def train_diff(self, state_diff, sai, reward,
                    skill_label, foci_of_attention):
         """
         Updates the state by some provided diff, then trains on the provided
@@ -53,7 +54,7 @@ class BaseAgent(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def train_last_state(self, selection, action, inputs, reward, skill_label,
+    def train_last_state(self, sai, reward, skill_label,
                          foci_of_attention):
         """
         Trains on the provided demonstration in the last / current state.
