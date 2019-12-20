210f94a3afb034b2e5fb27a78da34f5351076ae0
Ben Mastay <ben.mastay@soartech.com>
2019-12-20 14:09:46 -0500

    reformatting

--- django/apprentice_learner/views.py
+++ django/apprentice_learner/views.py
@@ -40,21 +40,23 @@ active_agent_id = None
 dont_save = False
 
 
-AGENTS = {'Stub': Stub,
-          'Memo': Memo,
-          'RLAgent': RLAgent,
-          'WhereWhenHowNoFoa': WhereWhenHowNoFoa,
-          'ModularAgent' : ModularAgent,
-          'SoartechAgent': SoarTechAgent}
+AGENTS = {
+    "Stub": Stub,
+    "Memo": Memo,
+    "RLAgent": RLAgent,
+    "WhereWhenHowNoFoa": WhereWhenHowNoFoa,
+    "ModularAgent": ModularAgent,
+    "SoartechAgent": SoarTechAgent,
+}
+
 
 def get_agent_by_id(id):
     global active_agent, active_agent_id, dont_save
-    if(id == active_agent_id):
+    if id == active_agent_id:
         agent = active_agent
     else:
         agent = Agent.objects.get(id=id)
     return agent
-
 
 
 def parse_operator_set(data, set_name, errs=None):
@@ -77,8 +79,7 @@ def parse_operator_set(data, set_name, errs=None):
             # This case should be impossible but I'm going to leave the error
             # catch in
             except MultipleObjectsReturned:
-                errs.append(
-                    "multiple operators with name {} exist".format(val))
+                errs.append("multiple operators with name {} exist".format(val))
         elif isinstance(val, int):
             try:
                 opr = Operator.objects.get(pk=val)
@@ -99,19 +100,19 @@ def create(http_request):
     if http_request.method != "POST":
         return HttpResponseNotAllowed(["POST"])
 
-    data = json.loads(http_request.body.decode('utf-8'))
+    data = json.loads(http_request.body.decode("utf-8"))
 
     errs = []
 
-    if 'agent_type' not in data or data['agent_type'] is None:
+    if "agent_type" not in data or data["agent_type"] is None:
         errs.append("request body missing 'agent_type'")
 
-    if not errs and data['agent_type'] not in AGENTS:
+    if not errs and data["agent_type"] not in AGENTS:
         errs.append("Specified agent not supported")
 
-    project_id = data.get('project_id', 1)
+    project_id = data.get("project_id", 1)
 
-    if project_id == 1 or project_id == '':
+    if project_id == 1 or project_id == "":
         project, created = Project.objects.get_or_create(id=1)
     else:
         try:
@@ -120,65 +121,67 @@ def create(http_request):
             errs.append(str.format("project: {} does not exist", project_id))
             project = None
 
-    if(data.get('no_ops_parse',True)):
-        if("feature_set" in data):
+    if data.get("no_ops_parse", True):
+        if "feature_set" in data:
             feature_set = data["feature_set"]
         else:
-            errs.append("Request body missing 'feature_set'. " + \
-             "Add it or set {'no_ops_parse':False} to draw from the database. ")
-        if("function_set" in data):
+            errs.append(
+                "Request body missing 'feature_set'. "
+                + "Add it or set {'no_ops_parse':False} to draw from the database. "
+            )
+        if "function_set" in data:
             function_set = data["function_set"]
         else:
-            errs.append("Request body missing 'function_set'. " + \
-             "Add it or set {'no_ops_parse':False} to draw from the database. ")
-        
+            errs.append(
+                "Request body missing 'function_set'. "
+                + "Add it or set {'no_ops_parse':False} to draw from the database. "
+            )
+
     else:
-        feature_set, errs = parse_operator_set(data, 'feature_set', errs)
-        function_set, errs = parse_operator_set(data, 'function_set', errs)
+        feature_set, errs = parse_operator_set(data, "feature_set", errs)
+        function_set, errs = parse_operator_set(data, "function_set", errs)
 
     if len(errs) > 0:
-        print('errors:\n {}'.format('\n'.join(errs)))
-        return HttpResponseBadRequest('errors: {}'.format(','.join(errs)))
+        print("errors:\n {}".format("\n".join(errs)))
+        return HttpResponseBadRequest("errors: {}".format(",".join(errs)))
 
-    if 'args' not in data:
+    if "args" not in data:
         args = {}
     else:
-        args = data['args']
+        args = data["args"]
 
-    args['feature_set'] = feature_set
-    args['function_set'] = function_set
-
+    args["feature_set"] = feature_set
+    args["function_set"] = function_set
 
     if project is not None:
-        args['feature_set'] += project.compile_features()
-        args['function_set'] += project.compile_functions()
+        args["feature_set"] += project.compile_features()
+        args["function_set"] += project.compile_functions()
 
     if settings.USE_CUSTOM_OPERATORS:
-        args['feature_set'] += custom_feature_set()
-        args['function_set'] += custom_function_set()
+        args["feature_set"] += custom_feature_set()
+        args["function_set"] += custom_function_set()
 
     try:
         # args['action_set'] = action_set
-        instance = AGENTS[data['agent_type']](**args)
-        agent_name = data.get('name', '')
-        agent = Agent(instance=instance,
-                      name=agent_name)
+        instance = AGENTS[data["agent_type"]](**args)
+        agent_name = data.get("name", "")
+        agent = Agent(instance=instance, name=agent_name)
         agent.save()
-        ret_data = {'agent_id': str(agent.id)}
+        ret_data = {"agent_id": str(agent.id)}
 
     except Exception as exp:
         traceback.print_exc()
         print("Failed to create agent", exp)
-        return HttpResponseServerError("Failed to create agent, "
-                                       "ensure provided args are "
-                                       "correct.")
+        return HttpResponseServerError(
+            "Failed to create agent, " "ensure provided args are " "correct."
+        )
 
     global active_agent, active_agent_id, dont_save
-    if(active_agent != None):
+    if active_agent != None:
         active_agent = None
         active_agent_id = None
         dont_save = False
-    if(str(data.get("stay_active",True)).lower() == 'true'):
+    if str(data.get("stay_active", True)).lower() == "true":
         active_agent = agent
         active_agent_id = str(agent.id)
         dont_save = str(data.get("dont_save", True)).lower() == "true"
@@ -199,18 +202,19 @@ def request(http_request, agent_id):
     try:
         if http_request.method != "POST":
             return HttpResponseNotAllowed(["POST"])
-        data = json.loads(http_request.body.decode('utf-8'))
+        data = json.loads(http_request.body.decode("utf-8"))
 
-        if 'state' not in data or data['state'] is None:
+        if "state" not in data or data["state"] is None:
             print("request body missing 'state'")
             return HttpResponseBadRequest("request body missing 'state'")
 
         agent = get_agent_by_id(agent_id)
         agent.inc_request()
-        response = agent.instance.request(data['state'])
+        response = agent.instance.request(data["state"])
 
         global dont_save
-        if(not dont_save): agent.save()
+        if not dont_save:
+            agent.save()
 
         # pr.disable()
         # pr.dump_stats("al.cprof")
@@ -223,7 +227,8 @@ def request(http_request, agent_id):
         # pr.disable()
         # pr.dump_stats("al.cprof")
         return HttpResponseServerError(str(exp))
-    
+
+
 @csrf_exempt
 def get_skills(http_request, agent_id):
     """
@@ -233,18 +238,19 @@ def get_skills(http_request, agent_id):
     try:
         if http_request.method != "POST":
             return HttpResponseNotAllowed(["POST"])
-        data = json.loads(http_request.body.decode('utf-8'))
+        data = json.loads(http_request.body.decode("utf-8"))
 
-        if 'states' not in data or data['states'] is None:
+        if "states" not in data or data["states"] is None:
             print("request body missing 'states'")
             return HttpResponseBadRequest("request body missing 'states'")
 
         agent = get_agent_by_id(agent_id)
         agent.inc_request()
-        response = agent.instance.get_skills(data['states'])
+        response = agent.instance.get_skills(data["states"])
 
         global dont_save
-        if(not dont_save): agent.save()
+        if not dont_save:
+            agent.save()
 
         # pr.disable()
         # pr.dump_stats("al.cprof")
@@ -257,6 +263,7 @@ def get_skills(http_request, agent_id):
         # pr.disable()
         # pr.dump_stats("al.cprof")
         return HttpResponseServerError(str(exp))
+
 
 @csrf_exempt
 def request_by_name(http_request, agent_name):
@@ -280,47 +287,56 @@ def train(http_request, agent_id):
     try:
         if http_request.method != "POST":
             return HttpResponseNotAllowed(["POST"])
-        data = json.loads(http_request.body.decode('utf-8'))
+        data = json.loads(http_request.body.decode("utf-8"))
 
         # print(data)
 
         errs = []
 
-        if 'state' not in data or data['state'] is None:
+        if "state" not in data or data["state"] is None:
             errs.append("request body missing 'state'")
-        if 'skill_label' not in data or data['skill_label'] is None:
-            data['skill_label'] = 'NO_LABEL'
-        if ('foci_of_attention' not in data or data['foci_of_attention'] is
-                None):
-            data['foci_of_attention'] = None
-        if 'selection' not in data or data['selection'] is None:
+        if "skill_label" not in data or data["skill_label"] is None:
+            data["skill_label"] = "NO_LABEL"
+        if "foci_of_attention" not in data or data["foci_of_attention"] is None:
+            data["foci_of_attention"] = None
+        if "selection" not in data or data["selection"] is None:
             errs.append("request body missing 'selection'")
-        if 'action' not in data or data['action'] is None:
+        if "action" not in data or data["action"] is None:
             errs.append("request body missing 'action'")
-        if 'inputs' not in data or data['inputs'] is None:
+        if "inputs" not in data or data["inputs"] is None:
             errs.append("request body missing 'inputs'")
-        if 'reward' not in data or data['reward'] is None:
-            if('correct' in data):
-                data['reward'] = 2*int(data['correct'] == True)-1
+        if "reward" not in data or data["reward"] is None:
+            if "correct" in data:
+                data["reward"] = 2 * int(data["correct"] == True) - 1
             else:
                 errs.append("request body missing 'reward'")
 
         # Linter was complaining about too many returns so I consolidated all
         # of the errors above
         if len(errs) > 0:
-            print('errors: {}'.format(','.join(errs)))
-            return HttpResponseBadRequest('errors: {}'.format(','.join(errs)))
+            print("errors: {}".format(",".join(errs)))
+            return HttpResponseBadRequest("errors: {}".format(",".join(errs)))
 
         agent = get_agent_by_id(agent_id)
         agent.inc_train()
 
-        sai = { 'selection': data['selection'], 'action': data['action'], 'input': data['inputs'] }
+        sai = {
+            "selection": data["selection"],
+            "action": data["action"],
+            "input": data["inputs"],
+        }
 
-        agent.instance.train(data['state'], data['next_state'],
-                             sai, data['reward'],
-                             data['skill_label'], data['foci_of_attention'])
+        agent.instance.train(
+            data["state"],
+            data["next_state"],
+            sai,
+            data["reward"],
+            data["skill_label"],
+            data["foci_of_attention"],
+        )
         global dont_save
-        if(not dont_save): agent.save()
+        if not dont_save:
+            agent.save()
 
         # pr.disable()
         # pr.dump_stats("al.cprof")
@@ -355,22 +371,22 @@ def check(http_request, agent_id):
     try:
         if http_request.method != "POST":
             return HttpResponseNotAllowed(["POST"])
-        data = json.loads(http_request.body.decode('utf-8'))
+        data = json.loads(http_request.body.decode("utf-8"))
 
         errs = []
 
-        if 'state' not in data:
+        if "state" not in data:
             errs.append("request body missing 'state'")
-        if 'selection' not in data:
+        if "selection" not in data:
             errs.append("request body missing 'selection'")
-        if 'action' not in data:
+        if "action" not in data:
             errs.append("request body missing 'action'")
-        if 'inputs' not in data:
+        if "inputs" not in data:
             errs.append("request body missing 'inputs'")
 
         if len(errs) > 0:
-            print('errors: {}'.format(','.join(errs)))
-            return HttpResponseBadRequest('errors: {}'.format(','.join(errs)))
+            print("errors: {}".format(",".join(errs)))
+            return HttpResponseBadRequest("errors: {}".format(",".join(errs)))
 
         agent = Agent.objects.get(id=agent_id)
         agent.inc_check()
@@ -378,10 +394,9 @@ def check(http_request, agent_id):
 
         response = {}
 
-        response['correct'] = agent.instance.check(data['state'],
-                                                   data['selection'],
-                                                   data['action'],
-                                                   data['inputs'])
+        response["correct"] = agent.instance.check(
+            data["state"], data["selection"], data["action"], data["inputs"]
+        )
         return HttpResponse(json.dumps(response))
 
     except Exception as exp:
@@ -413,13 +428,13 @@ def report(http_request, agent_id):
     agent = get_object_or_404(Agent, id=agent_id)
 
     response = {
-        'id': agent.id,
-        'name': agent.name,
-        'num_request': agent.num_request,
-        'num_train': agent.num_train,
-        'num_check': agent.num_check,
-        'created': agent.created,
-        'updated': agent.updated
+        "id": agent.id,
+        "name": agent.name,
+        "num_request": agent.num_request,
+        "num_train": agent.num_train,
+        "num_check": agent.num_check,
+        "created": agent.created,
+        "updated": agent.updated,
     }
 
     response = {k: str(response[k]) for k in response}
@@ -438,6 +453,6 @@ def report_by_name(http_request, agent_name):
 
 @csrf_exempt
 def test_view(http_request):
-    return render(http_request, 'apprentice_learner/tester.html',
-                  {'agents': Agent.objects.all()})
-
+    return render(
+        http_request, "apprentice_learner/tester.html", {"agents": Agent.objects.all()}
+    )
