fde0c127532a1bc1178ddce7acb2bd2323e81d55
eharpste <eharpste@cs.cmu.edu>
2020-01-09 15:00:35 -0500

    made changes to base agent to move toward a common API

--- apprentice/agents/base.py
+++ apprentice/agents/base.py
@@ -1,62 +1,41 @@
 from abc import ABCMeta, abstractmethod
 from typing import Dict
 from typing import Collection
-from apprentice.working_memory.representation import Skill
-
-from jsondiff import diff
 
 
 class BaseAgent(metaclass=ABCMeta):
-    prior_state = None
 
-    def __init__(self, prior_skills: Collection[Skill]):
+    def __init__(self, **kwargs):
         """
         Creates an agent with the provided skills.
         """
         pass
 
-    def request(self, state: Dict) -> Dict:
+    @abstractmethod
+    def request(self, state: Dict, **kwargs) -> Dict:
         """
         Returns a dict containing a Selection, Action, Input.
 
         :param state: a state represented as a dict (parsed from JSON)
         """
-        return self.request_diff(diff(state, self.prior_state))
-
-    @abstractmethod
-    def request_diff(self, state_diff: Dict) -> Dict:
-        """
-        :param diff: a diff object that is the output of JSON diff
-        """
         pass
 
+    @abstractmethod
     def train(self, state: Dict, selection: str, action: str, inputs:
-              Collection[str], reward: float, skill_label: str,
-              foci_of_attention: Collection[str]):
+              Collection[str], reward: float, **kwargs):
         """
         Accepts a JSON/Dict object representing the state, a string representing the
         skill label, a list of strings representing the foas, a string
         representing the selection, a string representing the action, list of
         strings representing the inputs, and a boolean correctness.
         """
-        return self.train_diff(diff(state, self.prior_state), selection,
-                               action, inputs, reward, skill_label,
-                               foci_of_attention)
-
-    @abstractmethod
-    def train_diff(self, state_diff, selection, action, inputs, reward,
-                   skill_label, foci_of_attention):
-        """
-        Updates the state by some provided diff, then trains on the provided
-        demonstration in this state.
-        """
         pass
 
     @abstractmethod
-    def train_last_state(self, selection, action, inputs, reward, skill_label,
-                         foci_of_attention):
+    def check(self, state: Dict, selection: str, action: str, inputs:
+              Collection[str], **kwargs) -> float:
         """
-        Trains on the provided demonstration in the last / current state.
+        Checks the correctness (reward) of an SAI action in a given state.
         """
         pass
 
