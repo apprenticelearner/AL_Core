57a96c0733a430880533cc34d026ab149b863576
robert.sheline <robert.sheline@soartech.com>
2020-01-13 13:38:17 -0500

    merge

--- apprentice/working_memory/adapters/experta_/factory.py
+++ apprentice/working_memory/adapters/experta_/factory.py
@@ -76,10 +76,12 @@ class ExpertaSkillFactory(Factory):
     def build(self, _condition: Any,
               _function: Callable,
               _name: str = None) -> Skill:
-        #if _name is None:
-            #_name = 'skill_' + str(uuid.uuid1())
 
         s = Skill(_condition, _function)#, _name)
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         return s
 
     def from_ex_rule(self, _rule: ex.Rule) -> Skill:
