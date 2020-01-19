592097d0a790cbaf64c6bc46c572234618fee01c
cmaclell <maclellan.christopher@gmail.com>
2020-01-19 14:30:24 -0500

    testing

--- apprentice/agents/soartech_agent.py
+++ apprentice/agents/soartech_agent.py
@@ -6,6 +6,7 @@ from typing import Dict
 from pprint import pprint
 
 from experta import KnowledgeEngine
+from experta import Fact
 
 from apprentice.agents.base import BaseAgent
 from apprentice.learners import WhenLearner
@@ -26,17 +27,21 @@ class SoarTechAgent(BaseAgent):
         self,
         wm: WorkingMemory = ExpertaWorkingMemory(ke=KnowledgeEngine()),
         when: WhenLearner = DQNLearner(),
-        epsilon: float = 0.3,
+        request_epsilon: float = 0.01,
+        train_epsilon: float = 0.3,
         action_penalty: float = -0.05,
         negative_actions: bool = False,
         skill_map: Dict[str, Skill] = fraction_skill_set,
         prior_skills: Collection[str] = frozenset([
-            'equal', 'add', 'multiply']),
-            # 'click_done', 'check',
-            #                                        'equal',
-            #                                        'update_answer',
-            #                                        'update_convert', 'add',
-            #                                        'multiply']),
+        #     'equal',
+        #     'add',
+        #     'multiply'
+        # ]),
+            'click_done', 'check',
+                                                   # 'equal',
+                                                   'update_answer',
+                                                   'update_convert', 'add',
+                                                   'multiply']),
         **kwargs
     ):
         # Just track the state as a set of Facts?
@@ -54,7 +59,6 @@ class SoarTechAgent(BaseAgent):
             self.working_memory = wm()
 
         print(prior_skills)
-        print(epsilon)
         if prior_skills is not None:
             prior_skills = [skill_map[s]
                             for s in prior_skills if s in skill_map]
@@ -66,17 +70,18 @@ class SoarTechAgent(BaseAgent):
         else:
             self.when_learning = None
 
-        self.epsilon = epsilon
+        self.request_epsilon = request_epsilon
+        self.train_epsilon = train_epsilon
         self.action_penalty = action_penalty
+        print('action_penalty', action_penalty)
         self.negative_actions = negative_actions
 
     def __deepcopy__(self, memo):
         log.debug("DEEP COPY NOT IMPLEMENTED -- RETURNING NONE!")
         return None
 
-    def select_activation(
-        self, candidate_activations: Collection[Activation]
-    ) -> Activation:
+    def select_activation(self, candidate_activations: Collection[Activation],
+                          is_train=False) -> Activation:
         """
         Given a list of candidate skills evaluate them and determines which one
         has the highest expected rewared in the current state.
@@ -96,7 +101,11 @@ class SoarTechAgent(BaseAgent):
         if self.when_learning is None:
             return random.choice(candidate_activations), 0
 
-        if random.random() < self.epsilon:
+        if not is_train and random.random() < self.request_epsilon:
+            print('random action')
+            return random.choice(candidate_activations), 0
+
+        if is_train and random.random() < self.train_epsilon:
             print('random action')
             return random.choice(candidate_activations), 0
 
@@ -126,6 +135,7 @@ class SoarTechAgent(BaseAgent):
         """
         # Just loads in the differences from the state diff
 
+        tabu = set()
         self.working_memory.update(state_diff)
 
         # This should do essentially what `engine.run` is doing from
@@ -152,13 +162,24 @@ class SoarTechAgent(BaseAgent):
             output = self.working_memory.activation_factory.to_ex_activation(
                 best_activation
             ).fire(self.working_memory.ke)
+            # pprint(self.working_memory.facts)
+            tabu.add((best_activation.get_rule_name(), frozenset(
+                best_activation.get_rule_bindings().items())))
 
             if isinstance(output, Sai):
                 break
 
             candidate_activations = [
-                activation for activation in self.working_memory.activations
+                activation for activation in
+                self.working_memory.activations
+                if (activation.get_rule_name(),
+                    frozenset(activation.get_rule_bindings().items())) not
+                in tabu
             ]
+            # pprint([{a: self.working_memory.facts[fid][a] for a in
+            #          self.working_memory.facts[fid] if a != "__factid__"}
+            #         for fid in self.working_memory.facts])
+            print("LENGTH OF ACTIVATIONS", len(candidate_activations))
             next_state = self.working_memory.state
 
             if self.when_learning:
@@ -185,6 +206,8 @@ class SoarTechAgent(BaseAgent):
         representing the selection, a string representing the action, list of
         strings representing the inputs, and a boolean correctness.
         """
+        tabu = set()
+
         if self.last_sai == sai and state_diff == {}:
             print("MATCHING LAST SAI")
             self.update_final(self.working_memory.state, reward,
@@ -203,6 +226,7 @@ class SoarTechAgent(BaseAgent):
         candidate_activations = [
             activation for activation in self.working_memory.activations
         ]
+        print("LENGTH OF ACTIVATIONS", len(candidate_activations))
 
         while True:
 
@@ -223,7 +247,7 @@ class SoarTechAgent(BaseAgent):
                     return {}
 
                 best_activation, expected_reward = self.select_activation(
-                    candidate_activations)
+                    candidate_activations, is_train=True)
 
                 # pprint(self.working_memory.state)
                 # print('firing', best_activation.get_rule_name())
@@ -232,6 +256,12 @@ class SoarTechAgent(BaseAgent):
                 output = self.working_memory.activation_factory.to_ex_activation(
                     best_activation
                 ).fire(self.working_memory.ke)
+                tabu.add((best_activation.get_rule_name(),
+                          frozenset(
+                              best_activation.get_rule_bindings().items())))
+                print(tabu)
+                print("TABU LENGTH", len(tabu))
+                # pprint(self.working_memory.facts)
 
                 if isinstance(output, Sai):
                     break
@@ -239,7 +269,16 @@ class SoarTechAgent(BaseAgent):
                 candidate_activations = [
                     activation for activation in
                     self.working_memory.activations
+                    if (activation.get_rule_name(),
+                        frozenset(activation.get_rule_bindings().items())) not
+                    in tabu
                 ]
+
+                print("LENGTH OF ACTIVATIONS", len(candidate_activations))
+                print([a.get_rule_name() for a in candidate_activations])
+                # pprint([{a: self.working_memory.facts[fid][a] for a in
+                #          self.working_memory.facts[fid] if a != "__factid__"}
+                #         for fid in self.working_memory.facts])
                 
                 next_state = self.working_memory.state
 
@@ -255,8 +294,13 @@ class SoarTechAgent(BaseAgent):
                 # log.debug('failed!')
                 print('failed!')
                 # print()
-                # candidate_activations = [act for act in candidate_activations
-                #                          if act != best_activation]
+                candidate_activations = [
+                    activation for activation in
+                    self.working_memory.activations
+                    if (activation.get_rule_name(),
+                        frozenset(activation.get_rule_bindings().items())) not
+                    in tabu
+                ]
 
                 # if the reward is positive, then we assume any other action is
                 # a negative example. So we train on the explity negatives.
@@ -290,6 +334,7 @@ class SoarTechAgent(BaseAgent):
             next_state = self.working_memory.state
             candidate_activations = [activation for activation in
                                      self.working_memory.activations]
+        print("LENGTH OF ACTIVATIONS", len(candidate_activations))
 
 
         if self.when_learning:
@@ -299,74 +344,6 @@ class SoarTechAgent(BaseAgent):
                 # next_state, []
                 next_state, candidate_activations
             )
-
-    def train_diff_old(self, state_diff, next_state_diff, sai, reward,
-                       skill_label, foci_of_attention):
-        """
-        Need the diff for the current state as well as the state diff for
-        computing the state that results from taking the action. This is
-        needed for performing Q learning.
-
-        Accepts a JSON object representing the state, a string representing the
-        skill label, a list of strings representing the foas, a string
-        representing the selection, a string representing the action, list of
-        strings representing the inputs, and a boolean correctness.
-        """
-        # relational inference step?
-        self.working_memory.update(state_diff)
-
-        state = None
-        sai_activation = None
-
-        while sai_activation is None:
-            candidate_activations = [
-                activation for activation in self.working_memory.activations
-            ]
-            if len(candidate_activations) == 0:
-                return
-            for activation in candidate_activations:
-                state = self.working_memory.state
-                output = self.working_memory.activation_factory.to_ex_activation(
-                    activation
-                ).fire(self.working_memory.ke)
-                if output == sai:
-                    sai_activation = activation
-                    break
-
-        if next_state_diff is None:
-            next_state = None
-            next_activations = []
-        else:
-            self.working_memory.update(next_state_diff)
-            next_state = self.working_memory.state
-            next_activations = list(self.working_memory.activations)
-
-        if self.when_learning and state and sai_activation:
-            self.when_learning.update(
-                state, sai_activation, reward, next_state, next_activations
-            )
-
-        # activation_sequence = None
-        # for act in self.working_memory.activations():
-        #    output = act.fire()
-        #    # from the method args.
-        #    if output == sai:
-        #        activation_sequence = [act]
-
-        # if activation_squence is None:
-        #    raise Exception("no explaination")
-
-        # if len(activation_sequence) == 1:
-        #    activation = activation_sequence[0]
-        # else:
-        #    # compile discovered activation seq into new skill and return
-        #    # activation of it
-        #    activation = self.how_learning(activation_sequence)
-
-        # activation has pointers to skill, state context, and match
-        # information; still working out what this interface looks like.
-        # activation.update_where(self.working_memory, reward)
-        # activation.update_when(self.working_memory, reward, next_state_diff)
 
     def train_last_state(self, *args):
         pass
