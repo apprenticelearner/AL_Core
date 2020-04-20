3567d7ea038635b838eca9274c6ecc77d9af33b2
robert.sheline <robert.sheline@soartech.com>
2020-04-19 22:58:39 -0400

    database setting, uses APPRENTICE_DB environment variable

--- django/agent_api/settings.py
+++ django/agent_api/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'agent_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	'OPTIONS': {'timeout': 100000} 
+if os.environ.get('APPRENTICE_DB', None) == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
