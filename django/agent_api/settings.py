ca82c7f3a6d8ecfef2bae1f73ba9b6f61650b097
Ben Mastay <ben.mastay@soartech.com>
2020-05-12 21:24:35 -0400

    add all allowed hosts

--- django/agent_api/settings.py
+++ django/agent_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g1s7^%a)(u(sy2l_flc@#5tm(w-@qu9)v^s(1dwyz9(-si_ca@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
